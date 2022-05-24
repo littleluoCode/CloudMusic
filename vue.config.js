e6808cb431f536227a5f57befd6aef45d5568559
littleluo <mclittleluo@outlook.com>
2022-05-25 00:51:17 +0800

    配置proxy代理跨域

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    lintOnSave: false
+    lintOnSave: false,
+    devServer: {
+        proxy: {
+            "/api": {
+                target: 'http://1.117.169.27:3300', //目标接口域名
+                secure: false,            //false为http访问，true为https访问
+                changeOrigin: true,      //是否跨域
+                //重写路径  
+                pathRewrite: {
+                    '^/api': 'http://1.117.169.27:3300'
+                },
+            },
+        }
+    }
 })
