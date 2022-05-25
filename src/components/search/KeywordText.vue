95157eb1d1b47c478b58ac980921320be089ab08
littleluo <mclittleluo@outlook.com>
2022-05-25 23:30:41 +0800

    搜索建议关键词高亮显示

---
+++ src/components/search/KeywordText.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "KeywordText",
+  props: {
+    keyword: {
+      type: String,
+      default: "",
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  render(h) {
+    const vnodes = [];
+    const text = this.text.split(this.keyword);
+    text.forEach((str) => {
+      vnodes.push(h("span", null, str));
+      vnodes.push(h("span", { style: "color: #1890ff" }, this.keyword));
+    });
+    if (text.length > 0) {
+      vnodes.pop();
+    }
+    return h("span", null, vnodes);
+  },
+};
+</script>
